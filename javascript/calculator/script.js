d9e8f9aa00ad5a695f2ae2882adfbe0b3930ca5e
sunalipatro <sunalipatro6@gmail.com>
2024-12-20 23:09:33 +0530

    PRE-KDU-JAVASCRIPT: Calculator implementation

---
+++ javascript/calculator/script.js
@@ -0,0 +1,52 @@
+const num1Field = document.getElementById("num1");
+const num2Field = document.getElementById("num2");
+const add = document.getElementById("add");
+const subtract = document.getElementById("subtract");
+const multiply = document.getElementById("multiply");
+const divide = document.getElementById("divide");
+
+
+function getNumbers(){
+  const num1 = parseFloat(num1Field.value);
+  const num2 = parseFloat(num2Field.value);
+
+  if(isNaN(num1) || isNaN(num2)){
+    alert("Please enter valid numbers.");
+    return null;
+  }
+
+  return {num1,num2};
+}
+
+
+add.addEventListener("click",()=>{
+  const numbers = getNumbers();
+  if(numbers){
+    alert(`Result: ${numbers.num1 + numbers.num2}`);
+  }
+})
+
+subtract.addEventListener("click", () => {
+  const numbers = getNumbers();
+  if (numbers) {
+    alert(`Result: ${numbers.num1 - numbers.num2}`);
+  }
+});
+
+multiply.addEventListener("click", () => {
+  const numbers = getNumbers();
+  if (numbers) {
+    alert(`Result: ${numbers.num1 * numbers.num2}`);
+  }
+});
+
+divide.addEventListener("click", () => {
+  const numbers = getNumbers();
+  if (numbers) {
+    if (numbers.num2 === 0) {
+      alert("Cannot divide by zero");
+    } else {
+      alert(`Result: ${numbers.num1 / numbers.num2}`);
+    }
+  }
+});
